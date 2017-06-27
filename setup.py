d11f247b22dfefd3263bfd7fc21f708ddc69ddda
Dror Atariah <d.atariah@rebuy.com>
2017-06-27 16:19:16 +0200

    Initial version of setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pubdsutils',
+      version='0.1',
+      description='Package of DS related public tools from reBuy.com',
+      author='Dror Atariah',
+      author_email='d.atariah@rebuy.com',
+      packages=['pubdsutils'],
+      zip_safe=False)
