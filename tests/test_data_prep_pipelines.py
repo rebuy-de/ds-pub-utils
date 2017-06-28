d61b4d1ffefa61af1a1f30317e17d57f47035e4c
Dror Atariah <d.atariah@rebuy.com>
2017-06-28 14:12:49 +0200

    Added minimal pipeline testing some transformers

---
+++ tests/test_data_prep_pipelines.py
@@ -0,0 +1,57 @@
+import unittest
+import pandas as pd
+import numpy as np
+from pandas.testing import assert_frame_equal
+from pubdsutils import data_preparation as dp
+from collections import OrderedDict
+from sklearn.pipeline import make_pipeline
+
+
+class TestSimplePipeline(unittest.TestCase):
+
+    def setUp(self):
+        self.df = pd.DataFrame(OrderedDict(
+            {
+                "v1": [2, 4, 6],
+                "v2": [10, 20, 30],
+                "d1": [
+                    pd.datetime(2017, 6, 27),
+                    pd.datetime(2017, 6, 24),
+                    pd.datetime(2017, 5, 1)
+                ],
+                "d2": [
+                    pd.datetime(2017, 6, 20),
+                    pd.datetime(2017, 6, 20),
+                    pd.datetime(2017, 4, 30)
+                ]
+            })
+        )
+
+    def test_simple_pipeline(self):
+        df_copy = self.df.copy()
+        pipeline = make_pipeline(
+            dp.RatioColumnToConst(col='v1', const=2, feat_name='v1_to_2'),
+            dp.RatioBetweenColumns(
+                numer='v2', denom='v1', feat_name='v2_to_v1')
+        )
+        res = pipeline.transform(self.df)
+        expected_res = pd.DataFrame(OrderedDict(
+            {
+                "v1": [2, 4, 6],
+                "v2": [10, 20, 30],
+                "d1": [
+                    pd.datetime(2017, 6, 27),
+                    pd.datetime(2017, 6, 24),
+                    pd.datetime(2017, 5, 1)
+                ],
+                "d2": [
+                    pd.datetime(2017, 6, 20),
+                    pd.datetime(2017, 6, 20),
+                    pd.datetime(2017, 4, 30)
+                ],
+                'v1_to_2': [1., 2., 3.],
+                'v2_to_v1': [5., 5., 5.]
+            }
+        ))
+        assert_frame_equal(res, expected_res)
+        assert_frame_equal(self.df, df_copy)
