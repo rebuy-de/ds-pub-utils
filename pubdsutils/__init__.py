a7962251c184b5d0ce6daf925f98f5e58ccd366e
Dror Atariah <d.atariah@rebuy.com>
2017-07-06 17:25:42 +0200

    Documented the utility functions in __init__.py

--- pubdsutils/__init__.py
+++ pubdsutils/__init__.py
@@ -1,4 +1,7 @@
 def _is_cols_subset_of_df_cols(cols, df):
+    """Utility function checking if column(s) in `cols` is/are a subset of the
+    the columns of `df`
+    """
     if set(cols).issubset(set(df.columns)):
         return True
     else:
@@ -7,6 +10,15 @@ def _is_cols_subset_of_df_cols(cols, df):
         )
 
 def _is_cols_input_valid(cols):
+    """Utility function checking the validity of the cols parameter.
+
+    The class `features_engineering` and `preprocessing` assume that the
+    parameter is:
+
+    - Not `None`
+    - Of type list having at least one item
+    - All items in the list are strings
+    """
     if cols is None or not isinstance(cols, list) or len(cols) == 0 or not all(isinstance(col, str) for col in cols):
         raise ValueError(
             "Cols should be a list of strings. Each string should correspond to a column name")
